6436b2e5cbb13bba994c1e6ddbe8b023e636129d
imendoza <imendozak24@gmail.com>
2020-08-05 05:37:46 -0500

    Update 1_install-fluentbit-kafka.sh

--- 1_install-fluentbit-kafka.sh
+++ 1_install-fluentbit-kafka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget -qO - https://packages.fluentbit.io/fluentbit.key
+wget -qO - https://packages.fluentbit.io/fluentbit.key | sudo apt-key add -
 echo "deb https://packages.fluentbit.io/ubuntu/bionic bionic main" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install td-agent-bit
